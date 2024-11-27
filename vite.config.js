f3bf9884e1ccc94e1d0291b54f58b2cac66d91cc
briendeau <yamori425@gmail.com>
2024-11-27 06:11:54 -0500

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-App-Mem-Game/", // Make sure this matches your repo name
 });
